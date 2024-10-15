7b610ede325a2de92be8fa87bfdd9f4eb56466b0
Shakil00111 <Shakil115204@gmail.com>
2024-10-15 10:28:22 +0600

    basic math operation

--- Basics/basic.cpp
+++ Basics/basic.cpp
@@ -2,11 +2,28 @@
 using namespace std;
 
 int main() {
+
+    //printing massages 
+
     cout<<"Hello world!"<<endl;
+
+
+    // using loop 
 
     for(int i = 0; i < 10; i++) {
         cout<< i << " ";
     }
+
+    cout<<endl;
+
+    // basic mathematical operations
+
+    int num1 = 789;
+    int num2 = 578;
+
+    int sum = num1 + num2;
+
+    cout<<"Sum of "<<num1<<" and "<<num2<<" is : "<<sum;
 
     return 0;
 }
