6be3a9e0c577289728e98deea34a29f550fc9720
Shakil00111 <Shakil115204@gmail.com>
2024-10-15 10:45:18 +0600

    add some  math operations

--- Basics/basic.cpp
+++ Basics/basic.cpp
@@ -10,7 +10,9 @@ int main() {
 
     // using loop 
 
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0 /*Initial value of i*/ ; i < 10 /*check i is greater or less than 10*/ ;
+     i++ /*incrment operator is always works at last.
+            when the loop compleate it's work it increament by one*/) {
         cout<< i << " ";
     }
 
@@ -18,12 +20,24 @@ int main() {
 
     // basic mathematical operations
 
-    int num1 = 789;
+    int num1 = 789; /*int is a data type evey number take palce under "int, float, double". */
     int num2 = 578;
 
     int sum = num1 + num2;
 
     cout<<"Sum of "<<num1<<" and "<<num2<<" is : "<<sum;
+
+    int sub = num1 - num2;
+
+    cout<<"Substitution of "<<num1<<" and "<<num2<<" is : "<<sub;
+
+    int multi = num1 * num2;
+
+    cout<<"Multiplication of "<<num1<<" and "<<num2<<" is : "<<multi;
+
+    float div = num1 / num2;
+
+    cout<<"Division of "<<num1<<" and "<<num2<<" is : "<<div;
 
     return 0;
 }
