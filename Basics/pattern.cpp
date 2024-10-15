eb44cd8e99727d0aa8eff28161bed388b7017a8f
Shakil00111 <Shakil115204@gmail.com>
2024-10-15 11:09:01 +0600

    normal pattern printing code

---
+++ Basics/pattern.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void lowerTrianguler(int row, int col);
+
+int main() {
+
+    int col = 0;
+    int row = 0;
+    
+    while(col <= 0 && row <= 0) {
+        cout<<"Column and Row should be greater than '1' "<<endl;
+
+        if(row <= 0) {
+            cout<<"Enter row : ";
+            cin>>row;
+        }
+
+        if(col <= 0) {
+            cout<<"Enter column: ";
+            cin>>col;
+        }
+    }
+
+    lowerTrianguler(row, col);
+
+    return 0;
+}
+
+void lowerTrianguler(int row, int col) {
+
+    cout<<"Lower Trianguler pattern is : "<<endl;
+
+    for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row && j <= i; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
