a4875827ca5587e2fe6ccaa79edef04e6bc60c43
Shakil00111 <Shakil115204@gmail.com>
2024-10-18 00:25:50 +0600

    add new code for prime number

---
+++ Basics/primeNumber.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void isPrime(int num);
+
+int main() {
+
+    int  num;
+
+    cout<<"Enter a number : ";
+    cin>>num;
+
+    isPrime(num);
+
+    return 0;
+}
+
+void isPrime(int num) {
+
+    if(num <= 0) {
+        cout<<"You must input a number greater than 0."<<endl;
+    } else {
+        if
+    }
+}
