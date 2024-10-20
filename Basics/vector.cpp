4d96079078b633cb13d85d2492db42c5631d9fc6
Shakil00111 <Shakil115204@gmail.com>
2024-10-20 23:56:53 +0600

    Vector Basic

--- Basics/vector.cpp
+++ Basics/vector.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main() {
 
-    // creating a vecotr list
+    // creating a linear vector array.
 
-    vector<int> arr[];
+    vector<int> arr; // infinite size vector array untill memory end.
+
+    arr.push_back(1); // use to insert element in a vector array.
+
+    arr[0] = 4; // we can change value after add using push_back() function but we can not insert initially without push_back().
+
+    cout<<arr[0]<<endl;
+    return 0;
 }
