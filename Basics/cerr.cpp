30c46d570871d6cd4f22ce4c7bf0bdd8bc7ce8b9
Shakil00111 <Shakil115204@gmail.com>
2025-07-04 19:41:53 +0600

    Add some more code for practice

--- Basics/cerr.cpp
+++ Basics/cerr.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
 
-    cerr << "Error: Something went wrong!" << endl;
+    int age;
+    string name;
+
+    cout << "Hello! What's your name? " << endl;
+    getline(cin, name);
+
+    cout << "Nice to meet you, Mr " << name << "!" << endl;
+    cout << "You're looking so young! How old are you? " << endl;
+    cin >> age;
+
+    if (age < 0) {
+        cerr << "Error: Age cannot be negative!" << endl;
+        return 1; // Exit with an error code
+    } else {
+        cout << "I'm lille surprised that you are " << age << " years old!" << endl;
+        cout << "You are still young!" << endl;
+    }
 
     return 0;
 }
