154abecd042ac6583cf49f7550864c424e3a5876
Shakil00111 <Shakil115204@gmail.com>
2025-07-06 12:36:09 +0600

    Different types of Initializations

---
+++ Basics/initialization.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // Initialize a variable with a value
+
+    // Initialize with brackets method
+    int x{10};
+
+    // Initialize with assignment operator
+    int y = 20;
+
+    // Initialize with function method
+    int z(30);
+
+    cout << x << endl << y << endl << z << endl;
+
+    return 0;
+}
