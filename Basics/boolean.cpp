f6b491a554c9ccbfef20a3163097be0d86fc5e29
Shakil00111 <Shakil115204@gmail.com>
2025-07-06 15:11:38 +0600

    Check boolalpha

---
+++ Basics/boolean.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    bool redLight{true};
+    bool greenLight{false};
+
+    cout << boolalpha;
+    cout<< "Red light: " << redLight << endl;
+    cout<< "Green light: " << greenLight << endl;   
+
+    return 0;
+}
