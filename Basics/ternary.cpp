c6121e3b52281400744d59346f8f7d3fb8ea8b3f
Shakil00111 <Shakil115204@gmail.com>
2025-07-09 15:09:12 +0600

    Ternary Operation example

---
+++ Basics/ternary.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // Ternary operator example
+    int a = 20, b = 30;
+    int max = (a > b) ? a : b;
+    cout << "The maximum value is: " << max << endl;
+
+    return 0;
+}
