b9c38c0c932df68c6ab9fb7de726553b2c8f439b
Shakil00111 <Shakil115204@gmail.com>
2025-07-11 01:20:03 +0600

    for loop

---
+++ Basics/forLoop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;    
+
+int main() {
+
+    int num = 1000;
+
+    for (unsigned int i = 0; i < num; i++) {
+        cout<< i + 1 << " Null" << endl;
+    }
+
+    return 0;
+}
