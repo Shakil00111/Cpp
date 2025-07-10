b00ccc53e25db47201c55307e25d760b674451af
Shakil00111 <Shakil115204@gmail.com>
2025-07-11 01:21:26 +0600

    while loop

---
+++ Basics/whileLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num = 1000;
+    unsigned int i = 0;
+
+    while (i < num) {
+        cout << i + 1 << " Null" << endl;
+        i++;
+    }
+
+    return 0;
+}
